3e2cf9b98e3c9c2e347a1db1c5c06c01615d12e7
adnansalehin <adnansalehin03@gmail.com>
2019-03-26 22:42:14 +0000

    made final score dynamic

---
+++ Assets/Source/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreUI;
+
+    // Use this for initializationvoid Start()
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        scoreUI.text = "Score: " + Airbase.Score;
+        Debug.Log(Airbase.Score);
+    }
+}
